a88968dc38bacefe988213b369c53c55f93cee97
kjsbot <wang.fiona03@gmail.com>
2022-07-24 19:25:23 -0400

    added files

--- youdemy/client/src/App.tsx
+++ youdemy/client/src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './styles/app.css';
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
 
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
@@ -9,19 +10,26 @@ import Login from './pages/Login';
 import Signup from './pages/Signup';
 import Header from './components/Header';
 
+const client = new ApolloClient({
+  uri: "http://localhost:4000", // url of backend
+  cache: new InMemoryCache() // used to cache query results after fetching them
+});
+
 function App() {
   return (
-    <div className="app">
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Landing />} />
-          <Route path="/home" element={[<Header />, <HomePage />]} />
-          <Route path="/new" element={[<Header />, <NewCourse />]} />
-          <Route path="/signup" element={[<Header />, <Signup />]} />
-          <Route path="/login" element={[<Header />, <Login />]} />
-        </Routes>
-      </BrowserRouter>
-    </div>
+    <ApolloProvider client={client}>
+      <div className="app">
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Landing />} />
+            <Route path="/home" element={[<Header />, <HomePage />]} />
+            <Route path="/new" element={[<Header />, <NewCourse />]} />
+            <Route path="/signup" element={[<Header />, <Signup />]} />
+            <Route path="/login" element={[<Header />, <Login />]} />
+          </Routes>
+        </BrowserRouter>
+      </div>
+    </ApolloProvider>
   );
 }
 
