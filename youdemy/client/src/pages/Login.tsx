f6a656e7443924fa0e0b5ac682795668667b7cb4
kjsbot <wang.fiona03@gmail.com>
2022-07-28 16:02:30 -0400

    Frontend - login and signup

--- youdemy/client/src/pages/Login.tsx
+++ youdemy/client/src/pages/Login.tsx
@@ -1,9 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
+import '../styles/userforms.css';
+import { Link } from 'react-router-dom';
 
 const Login = () => {
+    const [emailLogin, setEmailLogin] = useState<string>("");
+    const [passLogin, setPassLogin] = useState<string>("");
+
+    const handleEmailInputChange = (e: React.ChangeEvent<HTMLInputElement>) => setEmailLogin(e.target.value);
+    const handlePassInputChange = (e: React.ChangeEvent<HTMLInputElement>) => setPassLogin(e.target.value);
+
+    const submitSignupForm = () => {
+        console.log(emailLogin);
+        console.log(passLogin);
+    };
+
     return (
-        <div>
-            Login
+        <div className="user-form-div">
+            <form className='signup-form' autoComplete="off" onSubmit={submitSignupForm}>
+                <h1>Login</h1>
+                <input 
+                    className="email-input-box" 
+                    type="email" 
+                    placeholder="Email" 
+                    required
+                    onChange={handleEmailInputChange}
+                    value={emailLogin}
+                />
+                <input 
+                    className="pass-input-box" 
+                    type="password" 
+                    placeholder="Password" 
+                    required
+                    onChange={handlePassInputChange}
+                    value={passLogin}
+                />
+                <a href="#" className="reset-pass-link">Forgot your password?</a>
+                <button className="signup-btn" type="submit">
+                    Sign In
+                </button>
+                <div className="form-bottom-txt">
+                    No account? 
+                    <Link to="/signup"><span className="small-accent">Sign up!</span></Link>
+                </div>
+            </form>
         </div>
     );
 };
--- youdemy/client/src/pages/Signup.tsx
+++ youdemy/client/src/pages/Signup.tsx
@@ -1,9 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
+import '../styles/userforms.css';
+import { Link } from 'react-router-dom';
 
 const Signup = () => {
+    const [emailSignup, setEmailSignup] = useState<string>("");
+    const [passSignup, setPassSignup] = useState<string>("");
+
+    const handleEmailInputChange = (e: React.ChangeEvent<HTMLInputElement>) => setEmailSignup(e.target.value);
+    const handlePassInputChange = (e: React.ChangeEvent<HTMLInputElement>) => setPassSignup(e.target.value);
+
+    const submitSignupForm = () => {
+        console.log(emailSignup);
+        console.log(passSignup);
+    };
+
     return (
-        <div>
-            Signup
+        <div className="user-form-div">
+            <form className='signup-form' autoComplete="off" onSubmit={submitSignupForm}>
+                <h1>Signup</h1>
+                <input 
+                    className="email-input-box" 
+                    type="email" 
+                    placeholder="Email" 
+                    required
+                    onChange={handleEmailInputChange}
+                    value={emailSignup}
+                />
+                <input 
+                    className="pass-input-box" 
+                    type="password" 
+                    placeholder="Password" 
+                    required
+                    onChange={handlePassInputChange}
+                    value={passSignup}
+                />
+                <div className="space" />
+                <button className="signup-btn" type="submit">
+                    Create Account
+                </button>
+                <div className="form-bottom-txt">
+                    Have an account? 
+                    <Link to="/login"><span className="small-accent">Log in!</span></Link>
+                </div>
+            </form>
         </div>
     );
 };
